c3efe9a563f633ac4fac8302996aa7996c890a7d
senorbeast <hrishikesh99@hotmail.com>
2022-02-19 00:09:00 +0530

    init

---
+++ src/TileGrid/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function TileGrid() {
+  return (
+    <>
+    <div className = 'tileGrid'>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+        <div className="tile"></div>
+    </div> 
+    </>
+  )
+}
